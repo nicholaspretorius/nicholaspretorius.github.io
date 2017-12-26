eba9d1c00a3e92032617e03e2cc3f9851fc01e72
Nicholas Pretorius <nicholaspretorius@gmail.com>
2017-12-26 09:13:59 +0200

    Updates

--- src/pages/hello-world.md
+++
@@ -1,6 +0,0 @@
----
-title: "Hello World"
-date: "2017-12-03"
----
-
-This is my first blog post using Gatsby, React and Markdown!
--- src/utils/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from "typography";
-import wordpress2016Theme from "typography-theme-wordpress-2016";
-
-const typography = new Typography(wordpress2016Theme);
-
-export default typography;
