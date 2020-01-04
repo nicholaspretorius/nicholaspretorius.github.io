7db4e6a8270db920f8e8603025cce33b1141f1b6
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-04 19:33:53 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d4fc556ab1868dfb4751.js"
+    "url": "webpack-runtime-b9c90610a17b18f829c9.js"
   },
   {
     "url": "app-2df197290ad1ddff6bb4.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c68dc35882e86bf98b9b0e01b4868f76"
+    "revision": "32d3b0145f4604669c302bb9b4268a6a"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
